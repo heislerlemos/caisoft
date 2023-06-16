c64851aec7d81ac4110125ceb2abe147a34c0c77
heislerlemos <heisler.stlamo@gmail.com>
2023-06-16 14:58:43 +0100

    Added mx lookup

--- src/Layout.java
+++ src/Layout.java
@@ -1,11 +1,19 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.InetAddress;
 import javax.swing.JTextArea;
-
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import org.xbill.DNS.lookup.LookupSession;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Name;
+import org.xbill.DNS.MXRecord;
 
 public class Layout extends JFrame {
     public Layout() {
@@ -20,7 +28,7 @@ public class Layout extends JFrame {
 
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(550,250));
-        panel.setBackground(Color.gray);
+        panel.setBackground(new Color(1, 50, 32));
         panel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 10));
 
 
@@ -30,9 +38,10 @@ public class Layout extends JFrame {
 
 
         JLabel name = new JLabel("Insira o registo de dns  :");
+        name.setForeground(Color.WHITE);
         panel.add(name);
 
-        JTextField fieldname = new JTextField(10);
+        JTextField fieldname = new JTextField(15);
         panel.add(fieldname);
 
         JButton b = new JButton("Procurar o registo");
@@ -42,17 +51,48 @@ public class Layout extends JFrame {
         panel.add(area);
         panel.setSize(300,400);
 
-
+    // Build a port tester with telnet 
         b.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 System.out.println("Serching for the record ip : " + fieldname.getText());
+
+
                 try {
                     InetAddress address = InetAddress.getByName(fieldname.getText());
                     System.out.println("This is the record " + address);
                     //JOptionPane.showMessageDialog(null, address);
-                    area.setText("host value " + address);
+
+
+
+
+
+                    LookupSession s = LookupSession.defaultBuilder().build();
+                    Name mxLookup = Name.fromString(fieldname.getText());
+                    s.lookupAsync(mxLookup, org.xbill.DNS.Type.MX)
+                            .whenComplete(
+                                    (answers, ex) -> {
+                                        if (ex == null) {
+                                            if (answers.getRecords().isEmpty()) {
+                                                System.out.println(mxLookup + " has no MX");
+                                                area.setText(mxLookup + " has no MX" );
+
+                                            } else {
+                                                for (Record rec : answers.getRecords()) {
+                                                    MXRecord mx = ((MXRecord) rec);
+                                                    System.out.println(
+                                                            "Host " + mx.getTarget() + " has preference " + mx.getPriority());
+                                                    area.setText("IP do Host : " + address +  "\n"  + "Email do Host : " + mx.getTarget()  );
+                                                }
+                                            }
+                                        } else {
+                                            ex.printStackTrace();
+                                        }
+                                    })
+                            .toCompletableFuture()
+                            .get();
+
 
 
                 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import org.xbill.DNS.*;
-import java.net.InetAddress;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
